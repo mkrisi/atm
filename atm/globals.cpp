422bc046e0831c4f98cf4b7cf7cccb229d46b56f
Kriszke <Kriszke@DESKTOP-T83KGKU>
2017-03-06 22:50:52 +0100

    En ezt csinaltam, szupi

--- atm/globals.cpp
+++ atm/globals.cpp
@@ -4,3 +4,4 @@ using std::string;
 int cr = 1, c, csz = 0, bsze = 1;
 string nm = {}, pwd = {}, pwdu = {}, bsz = {}, fn = {}, az = {}, pwdaz = {}, nye = {};
 int ezegyvizprogintegervaltozo = 0;
+int ezegykrisztianfelevaltozo = 100;
